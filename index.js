d493f4a30f82562a123e31f3f3592ef95056ba58
Yomesh Gupta <yomesh.gupta@gmail.com>
2022-05-10 20:42:18 +0530

    Rewrote

---
+++ index.js
@@ -0,0 +1,16 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const ecommerceRouter = jsonServer.router("ecommerce.json");
+const middlewares = jsonServer.defaults({
+  readOnly: true,
+});
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+
+// Use default router
+server.use("/api/ecommerce", ecommerceRouter);
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
